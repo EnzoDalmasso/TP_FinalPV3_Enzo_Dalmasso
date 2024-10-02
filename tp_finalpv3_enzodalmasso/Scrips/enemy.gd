5cbcf8079a979a47e3c784c1b12968b644c20db6
Enzo Dalmasso <dalmassoenzo6@gmail.com>
2024-10-02 20:16:36 -0300

    Colision enemigo a player
    Se realizo la primera parte de colisión del enemigo al Player

--- tp_finalpv3_enzodalmasso/Scrips/enemy.gd
+++ tp_finalpv3_enzodalmasso/Scrips/enemy.gd
@@ -3,11 +3,14 @@ class_name enemigo_base
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var cuadro_colision: Area2D = $CuadroColision
+@onready var areaespada: CollisionShape2D = $ColisionEspada/CollisionShape2D
 
 @export_range(10,100) var vel: int = 10 
 var player = null
 var direction = Vector2.ZERO
 @export var vida = 1
+var attack : bool = false
+
 
 func _physics_process(_delta):
 	
@@ -23,22 +26,31 @@ func _on_area_2d_body_entered(body):
 		if body.get_collision_layer() == 2:
 			player=body
 			set_run(true)
-		
+			#set_attack(false)
 
 
 func _on_area_2d_body_exited(_body):
 		player = null
 		set_run(false)
-		
+		#set_attack(false)
+
 
  
 func set_run(value):
 	animation_tree["parameters/conditions/Run"] = value
 	animation_tree["parameters/conditions/Idle"] = not value
- 
+
+
+func set_swing(value = false):
+	attack = value
+	animation_tree["parameters/conditions/Swing"] = value
+	if value == false:
+		areaespada.disabled = true
+
 func update_blend_position():
 	animation_tree["parameters/idle/blend_position"] = direction
 	animation_tree["parameters/run/blend_position"] = direction
+	animation_tree["parameters/attack/blend_position"] = direction
 
 func danio():
 	vida-=1
@@ -48,7 +60,10 @@ func danio():
 
 
 
-
+#Cuando el Player entra al CuadroColision el enemigo realiza el attaque 
 func _on_cuadro_colision_body_entered(body):
 	if body is player:
+		set_swing(true)
 		body.danio()
+
+
