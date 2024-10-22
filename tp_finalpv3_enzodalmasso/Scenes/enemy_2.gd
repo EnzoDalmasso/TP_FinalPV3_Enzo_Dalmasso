d047ee1e229922b43c516643474c2aee912b3d4a
Enzo Dalmasso <dalmassoenzo6@gmail.com>
2024-10-22 19:22:01 -0300

    Animaciones Enemigo 2
    Se agregaron animaciones al enemigo 2

--- tp_finalpv3_enzodalmasso/Scenes/enemy_2.gd
+++
@@ -1,2 +0,0 @@
-extends enemigo_base
-
---
+++ tp_finalpv3_enzodalmasso/Scrips/enemy_2.gd
@@ -0,0 +1,88 @@
+extends enemigo_base
+
+
+func _physics_process(_delta):
+	
+	if player_ref !=null and perseguir:
+		
+		direction = player_ref.position-position
+		direction= direction.normalized()
+		velocity = direction * vel
+		update_blend_position()
+		move_and_slide()
+
+
+func _on_area_2d_body_entered(body):
+		if body.get_collision_layer() == 2:
+			perseguir = true
+			player_ref=body
+			set_run(true)
+
+
+func _on_area_2d_body_exited(_body):
+		perseguir=false
+		player_ref = null
+		set_run(false)
+
+
+ 
+func set_run(value):
+	animation_tree["parameters/conditions/Run"] = value
+	animation_tree["parameters/conditions/Idle"] = not value
+	#print("corre perra correee: ", value)
+
+
+func set_swing(value = false):
+	animation_tree["parameters/conditions/Swing"] = value
+
+func set_dead(value = false):
+	animation_tree["parameters/conditions/Dead"] = value
+
+func update_blend_position():
+	animation_tree["parameters/idle/blend_position"] = direction
+	animation_tree["parameters/run/blend_position"] = direction
+	animation_tree["parameters/attack/blend_position"] = direction
+	animation_tree["parameters/dead/blend_position"] = direction
+
+func danio():
+	vida-=1
+	if vida == 0:
+		print("muerto")
+		set_physics_process(false)
+		set_dead(true)
+		set_run(false)
+		set_swing(false)
+		await (animation_tree.animation_finished)
+		queue_free()
+
+
+
+
+#Cuando el Player entra al CuadroColision el enemigo realiza la animacion de ataque y deja de correr
+func _on_cuadro_colision_body_entered(body):
+	if body is Player:
+		perseguir=false
+		set_run(false)
+		set_swing(true)
+		
+
+#Cuando el Player entra al CuadroColision el enemigo deja de realizar la animacion de ataque y vuelve al estado correr
+func _on_cuadro_colision_body_exited(body: Node2D) -> void:
+	if body is Player:
+		set_swing(false)
+		set_run(true)
+		perseguir=true
+
+
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	if !perseguir:
+		if anim_name=="attack_left" or anim_name=="attack_right"or anim_name=="attack_up" or anim_name=="attack_down":
+			set_swing(true)
+	
+
+
+
+func _on_colision_espada_body_entered(body):
+	if body is Player:
+		player_ref.danio()
