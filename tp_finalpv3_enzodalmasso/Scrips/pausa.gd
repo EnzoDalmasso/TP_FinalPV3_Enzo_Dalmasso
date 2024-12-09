e3a1b13b9f7199916cb6fcff41a13146ebe5bc64
Enzo Dalmasso <dalmassoenzo6@gmail.com>
2024-12-09 17:54:44 -0300

    Boton Muteo
    Se agrega un boton de muteo de musica de fondo

--- tp_finalpv3_enzodalmasso/Scrips/pausa.gd
+++ tp_finalpv3_enzodalmasso/Scrips/pausa.gd
@@ -1,6 +1,6 @@
 extends Control
 @onready var sonido_click: AudioStreamPlayer2D = $SonidoClick
-
+var audio = AudioServer.get_bus_index("Master")
 
 signal pausa
 
@@ -19,3 +19,8 @@ func _on_button_2_pressed() -> void:
 func _on_h_scroll_bar_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(0,value)
 
+
+
+
+func _on_mute_pressed() -> void:
+		AudioServer.set_bus_mute(audio, not AudioServer.is_bus_mute(audio))
